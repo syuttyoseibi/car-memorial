fa8d61f5c31fa83768af8bbbef292e0ecc2fa8c6
syuttyoseibi <daishi2matsuda@gmail.com>
2025-08-10 19:09:37 +0900

    Fix Vercel routing and server configuration

--- server.js
+++ server.js
@@ -101,6 +101,4 @@ app.post('/download-pdf', async (req, res) => {
     }
 });
 
-app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
-});
+module.exports = app;
